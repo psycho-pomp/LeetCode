eeada80d2ce2933c01035a7708acf646a614582d
psycho-pomp <52618538+psycho-pomp@users.noreply.github.com>
2023-03-02 16:18:16 +0530

    Time: 29 ms (31.51%), Space: 23.2 MB (50.77%) - LeetHub

--- 344-reverse-string/344-reverse-string.cpp
+++ 344-reverse-string/344-reverse-string.cpp
@@ -3,9 +3,7 @@ public:
     void reverseString(vector<char>& s) {
         int start=0,end=s.size()-1;
         while(start<end){
-            int temp=s[start];
-            s[start]=s[end];
-            s[end]=temp;
+            swap(s[start],s[end]);
             start++;
             end--;
         }
