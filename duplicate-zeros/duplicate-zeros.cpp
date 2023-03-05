0166bc7c88f1f11450d64565beddb94c2cf57a79
psycho-pomp <52618538+psycho-pomp@users.noreply.github.com>
2023-03-06 01:33:15 +0530

    Time: 119 ms, Memory: 9.5 MB - LeetHub

--- duplicate-zeros/duplicate-zeros.cpp
+++ duplicate-zeros/duplicate-zeros.cpp
@@ -5,11 +5,10 @@ public:
         while(i<n){
             if(arr[i]==0){
                 int j=n-1;
-                while(j>i+1){
+                while(j>i){
                     arr[j]=arr[j-1];
                     j--;  
                 }
-                if(i+1<n) arr[i+1]=0;
                 i++;
             }
             i++;
