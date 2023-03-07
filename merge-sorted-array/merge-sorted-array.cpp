1553f846985b56a8443b050bc59e1b3e6da63fbd
psycho-pomp <52618538+psycho-pomp@users.noreply.github.com>
2023-03-07 16:48:36 +0530

    Time: 0 ms, Memory: 9.1 MB - LeetHub

--- merge-sorted-array/merge-sorted-array.cpp
+++ merge-sorted-array/merge-sorted-array.cpp
@@ -4,13 +4,6 @@ public:
         int k=m+n-1;
         int i=m-1;
         int j=n-1;
-        if(m==0){
-            while(k>=0){
-                nums1[k]=nums2[j];
-                j--;
-                k--;
-            }
-        }
         while(k>=0 && i>=0 && j>=0){
             if(nums2[j]<nums1[i]){
                 nums1[k]=nums1[i];
@@ -25,17 +18,10 @@ public:
             }
             
         }
-        //cout<<i<<" "<<k<<endl;
         while(j>=0){
             nums1[k]=nums2[j];
             j--;
             k--;    
         }
-        while(i>=0){
-            nums1[k]=nums1[i];
-            i--;
-            k--;    
-        }
-        
     }
 };
