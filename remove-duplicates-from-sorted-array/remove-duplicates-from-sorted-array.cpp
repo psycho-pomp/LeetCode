64bb20a7e28a5003622d7b573427972074558783
psycho-pomp <52618538+psycho-pomp@users.noreply.github.com>
2023-03-17 00:15:17 +0530

    Time: 20 ms, Memory: 18.5 MB - LeetHub

--- remove-duplicates-from-sorted-array/remove-duplicates-from-sorted-array.cpp
+++ remove-duplicates-from-sorted-array/remove-duplicates-from-sorted-array.cpp
@@ -1,13 +1,14 @@
 class Solution {
 public:
     int removeDuplicates(vector<int>& nums) {
-        int index=1;
+        int k=1;
         for(int i=1;i<nums.size();i++){
-            if(nums[index-1]!=nums[i]){
-                nums[index]=nums[i];
-                index++;
+            if(nums[i]!=nums[i-1]){
+                nums[k]=nums[i];
+                k++;
             }
+            
         }
-        return index;
+        return k;
     }
 };
