f6fc6cc2391c8317b73cfdcaf30903c5e5be585d
psycho-pomp <52618538+psycho-pomp@users.noreply.github.com>
2023-09-01 20:31:24 +0530

    Time: 6 ms (56.68%), Space: 7.7 MB (89.73%) - LeetHub

--- 0338-counting-bits/0338-counting-bits.cpp
+++ 0338-counting-bits/0338-counting-bits.cpp
@@ -1,14 +1,14 @@
 class Solution {
 public:
     vector<int> countBits(int n) {
-        vector<int> vp;
-        //vp.push_back(0);
-        for(int i=0;i<=n;i++){
-
-            vp.push_back(__builtin_popcount(i));
-
-        }
-        return vp;
+        vector<int> ans(n+1,0);
+        for(int i=1 ; i<=n ; i++){
+           if(i%2 == 0){
+               ans[i] = ans[i/2];
+           }else
+               ans[i]=ans[i-1]+1;
+            }
+            return ans;
         
     }
 };
