c47ce992816f0e9563133fc7c1b6859928cfa062
psycho-pomp <52618538+psycho-pomp@users.noreply.github.com>
2023-09-05 22:38:54 +0530

    Time: 8 ms (47.40%), Space: 11.7 MB (8.55%) - LeetHub

---
+++ 0138-copy-list-with-random-pointer/0138-copy-list-with-random-pointer.cpp
@@ -0,0 +1,47 @@
+/*
+// Definition for a Node.
+class Node {
+public:
+    int val;
+    Node* next;
+    Node* random;
+    
+    Node(int _val) {
+        val = _val;
+        next = NULL;
+        random = NULL;
+    }
+};
+*/
+
+class Solution {
+public:
+    Node* copyRandomList(Node* head) {
+        if(head== NULL) return NULL;
+        Node* headDeepCopy=new Node(head->val);
+        Node* prev=headDeepCopy;
+        Node* temp=head;
+        map<Node*,Node*>mp;
+        while(temp->next){
+            cout<<temp->val<<endl;
+            mp[temp]=prev;
+            temp=temp->next;
+            Node* temp1=new Node(temp->val);
+            prev->next=temp1;
+            prev=temp1;   
+        }
+        mp[temp]=prev;
+        temp=head;
+        prev=headDeepCopy;
+        // for(auto x:mp){
+        //     cout<<x.first->val<<" "<<x.second->val<<endl;
+        // }
+        while(temp){
+            prev->random=mp[temp->random];
+            temp=temp->next;
+            prev=prev->next;  
+        }
+        return headDeepCopy;
+        
+    }
+};
