68dec749b9d3381e6184648ddd1b40ba0cc364d4
psycho-pomp <52618538+psycho-pomp@users.noreply.github.com>
2023-09-22 00:15:36 +0530

    Time: 23 ms (76.84%), Space: 89.8 MB (33.58%) - LeetHub

--- 0004-median-of-two-sorted-arrays/0004-median-of-two-sorted-arrays.cpp
+++ 0004-median-of-two-sorted-arrays/0004-median-of-two-sorted-arrays.cpp
@@ -1,32 +1,44 @@
 class Solution {
 public:
     double findMedianSortedArrays(vector<int>& nums1, vector<int>& nums2) {
-        int n = nums1.size();
-        int m = nums2.size();
-        int i = 0, j = 0, m1 = 0, m2 = 0;
-
-        // Find median.
-        for (int count = 0; count <= (n + m) / 2; count++) {
-            m2 = m1;
-            if (i != n && j != m) {
-                if (nums1[i] > nums2[j]) {
-                    m1 = nums2[j++];
-                } else {
-                    m1 = nums1[i++];
-                }
-            } else if (i < n) {
-                m1 = nums1[i++];
-            } else {
-                m1 = nums2[j++];
+        int n1=nums1.size();
+        int n2=nums2.size();
+        double res=0;
+        int k=(n1+n2)/2+1;
+        int i=0,j=0,z=0;
+        int curr,prev;
+        while(i<n1 && j<n2 && z<k){
+            prev=curr;
+            if(nums1[i]<nums2[j]){
+                curr=nums1[i];
+                i++;
             }
+            else{
+                curr=nums2[j];
+                j++;
+            }
+            z++;
+            //cout<<curr<<endl;
         }
-
-        // Check if the sum of n and m is odd.
-        if ((n + m) % 2 == 1) {
-            return static_cast<double>(m1);
-        } else {
-            double ans = static_cast<double>(m1) + static_cast<double>(m2);
-            return ans / 2.0;
+        // cout<<z<<" "<<k<<endl;
+        // cout<<curr<<" "<<prev<<endl;
+        if(z<k){
+            while(i<n1 && z<k){
+                prev=curr;
+                curr=nums1[i];
+                i++;
+                z++;
+            }
+            while(j<n2 && z<k){
+                prev=curr;
+                curr=nums2[j];
+                j++;
+                z++;
+            }
+            cout<<z<<" "<<k<<endl;
+            cout<<curr<<" "<<prev<<endl;
         }
+        (n1+n2)%2==0? res=(curr+prev)/2.0 : res=curr;
+        return res;
     }
 };
