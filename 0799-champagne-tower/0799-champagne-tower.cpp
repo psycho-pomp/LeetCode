c19cf0fbc00ad66c7fdece3f40105939dfa81b63
psycho-pomp <52618538+psycho-pomp@users.noreply.github.com>
2023-09-24 23:57:57 +0530

    Time: 13 ms (32.38%), Space: 14.4 MB (40.37%) - LeetHub

--- 0799-champagne-tower/0799-champagne-tower.cpp
+++ 0799-champagne-tower/0799-champagne-tower.cpp
@@ -5,7 +5,7 @@ public:
         for(int i=0;i<=query_row;i++){
             vector<double>nextRow(i+2,0);
             for(int j=0;j<=i;j++){
-                if(currRow[j]>1){
+                if(currRow[j]>=1){
                     nextRow[j]+=(currRow[j]-1)/2.0;
                     nextRow[j+1]+=(currRow[j]-1)/2.0;
                     currRow[j]=1;    
