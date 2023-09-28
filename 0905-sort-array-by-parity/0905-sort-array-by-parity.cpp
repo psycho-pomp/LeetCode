9da19ec188a7d0bf6692189a8d98dfa776d8adf9
psycho-pomp <52618538+psycho-pomp@users.noreply.github.com>
2023-09-28 21:13:01 +0530

    Time: 10 ms (32.57%), Space: 16.6 MB (25.63%) - LeetHub

--- 0905-sort-array-by-parity/0905-sort-array-by-parity.cpp
+++ 0905-sort-array-by-parity/0905-sort-array-by-parity.cpp
@@ -5,7 +5,6 @@ public:
         
         for (int i = 0; i < nums.size(); i++) {
             if (nums[i] % 2 == 0) {
-                // Swap the current element with the next available even-indexed position
                 swap(nums[i], nums[evenIndex++]);
             }
         }
