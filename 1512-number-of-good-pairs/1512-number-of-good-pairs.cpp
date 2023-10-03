7055420d47a27aa7eb8dd0e99dd963337ae8ee00
psycho-pomp <52618538+psycho-pomp@users.noreply.github.com>
2023-10-03 16:09:53 +0530

    Time: 0 ms (100.00%), Space: 7.5 MB (16.85%) - LeetHub

--- 1512-number-of-good-pairs/1512-number-of-good-pairs.cpp
+++ 1512-number-of-good-pairs/1512-number-of-good-pairs.cpp
@@ -7,16 +7,6 @@ public:
             ump[value]+=1;
             res+=ump[value]-1;
         }
-        
-        // for(auto mp:ump){
-        //     cout<<mp.second<<",";
-        //     int curr=1;
-        //     for(int i=2;i<mp.second;i++){
-        //         curr+=i;
-        //     }
-        //     cout<<curr<<endl;
-        //     res+=curr;
-        // }
         return res;
         
     }
