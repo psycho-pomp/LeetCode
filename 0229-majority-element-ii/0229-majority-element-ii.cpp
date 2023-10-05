0756164629e46466ce79f5fe684d669b6fd167a0
psycho-pomp <52618538+psycho-pomp@users.noreply.github.com>
2023-10-05 14:31:18 +0530

    Time: 11 ms (51.76%), Space: 16.4 MB (9.38%) - LeetHub

--- 0229-majority-element-ii/0229-majority-element-ii.cpp
+++ 0229-majority-element-ii/0229-majority-element-ii.cpp
@@ -3,7 +3,7 @@ public:
     vector<int> majorityElement(vector<int>& nums) {
         int occured=nums.size()/3;
         vector<int> res;
-        cout<<occured<<endl;
+        //cout<<occured<<endl;
         unordered_map<int,int>ump;
         for(auto val:nums){
             ump[val]+=1;
