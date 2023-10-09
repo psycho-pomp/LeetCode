4d4b149b6979ead01157e2bfefe79b98c12ccdf0
psycho-pomp <52618538+psycho-pomp@users.noreply.github.com>
2023-10-09 21:46:12 +0530

    Time: 3 ms (91.96%), Space: 14.2 MB (8.57%) - LeetHub

--- 0034-find-first-and-last-position-of-element-in-sorted-array/0034-find-first-and-last-position-of-element-in-sorted-array.cpp
+++ 0034-find-first-and-last-position-of-element-in-sorted-array/0034-find-first-and-last-position-of-element-in-sorted-array.cpp
@@ -2,24 +2,12 @@ class Solution {
 public:
     vector<int> searchRange(vector<int>& arr, int target) {
         int start=0,end=arr.size()-1;
-        int min_idx=INT_MAX;
-        int max_idx=INT_MIN;
+        int first=-1,last=-1;
         while(start<=end){
-            if(arr[start]==target){
-                max_idx=max(max_idx,start);
-                min_idx=min(min_idx,start);
-            }
-            if(arr[end]==target){
-                max_idx=max(max_idx,end);
-                min_idx=min(min_idx,end);
-            }
             int mid=(start+end)/2;
-            //cout<<mid<<endl;
             if(arr[mid]==target){
-                max_idx=max(max_idx,mid);
-                min_idx=min(min_idx,mid);
-                start+=1;
-                end-=1;
+                end=mid-1;
+                first=mid;
             }
             else if(arr[mid]>target){
                 end=mid-1;
@@ -28,7 +16,20 @@ public:
                 start=mid+1;
             }
         }
-        if(min_idx==INT_MAX && max_idx==INT_MIN) return {-1,-1};
-        return {min_idx,max_idx};
+        start=0,end=arr.size()-1;
+        while(start<=end){
+            int mid=(start+end)/2;
+            if(arr[mid]==target){
+                start=mid+1;
+                last=mid;
+            }
+            else if(arr[mid]>target){
+                end=mid-1;
+            }
+            else{
+                start=mid+1;
+            }
+        }
+        return {first,last};
     }
 };
