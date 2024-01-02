c3c9f0f69fa196100984c559b28b5f65dd99894c
psycho-pomp <52618538+psycho-pomp@users.noreply.github.com>
2024-01-02 22:48:37 +0530

    Time: 15 ms (97.60%), Space: 17.7 MB (75.65%) - LeetHub

---
+++ 0455-assign-cookies/0455-assign-cookies.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    int findContentChildren(vector<int>& g, vector<int>& s) {
+        sort(g.begin(),g.end());
+        sort(s.begin(),s.end());
+        int ans=0,j=0;
+        for(int val:g){
+            //cout<<j<<endl;
+            for(;j<s.size();j++){
+                if(s[j]>=val){
+                    ans+=1;
+                    j+=1;
+                    break;
+                }
+            }
+        }
+        return ans;
+        
+    }
+};
