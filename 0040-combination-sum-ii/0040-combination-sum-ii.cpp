c139137ce5d75486c659fcb9239ecf685fd43aae
Abhishek Anand <52618538+psycho-pomp@users.noreply.github.com>
2024-01-14 16:22:53 +0530

    Time: 3 ms (86.78%), Space: 11 MB (78.04%) - LeetHub

--- 0040-combination-sum-ii/0040-combination-sum-ii.cpp
+++ 0040-combination-sum-ii/0040-combination-sum-ii.cpp
@@ -2,24 +2,18 @@ class Solution {
 public:
     void getCombinationSum2(int i,int n,vector<int>& candidates, int target,vector<int>& temp,vector<vector<int>>& ans) {
         
-        if(i==n){
-            if(target==0){
-                ans.push_back(temp);
-            }
+        
+        if(target==0){
+            ans.push_back(temp);
             return;
         }
-        if(target>=candidates[i]){
-            temp.push_back(candidates[i]);
-            getCombinationSum2(i+1,n,candidates,target-candidates[i],temp,ans);
+        for(int idx=i;idx<n;idx++){
+            if(idx>i && candidates[idx]==candidates[idx-1]) continue;
+            if(target<candidates[idx]) break;
+            temp.push_back(candidates[idx]);
+            getCombinationSum2(idx+1,n,candidates,target-candidates[idx],temp,ans);
             temp.pop_back();
         }
-        int currIdx=i+1;
-        while(currIdx<n && candidates[currIdx]==candidates[i]){
-            currIdx++;
-            
-        }
-        //cout<<currIdx<<endl;
-        getCombinationSum2(currIdx,n,candidates,target,temp,ans);
         
     }
     vector<vector<int>> combinationSum2(vector<int>& candidates, int target) {
