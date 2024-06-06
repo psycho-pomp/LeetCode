994259a9174747d4ec32a47b8724196222de84c9
Abhishek Anand <52618538+psycho-pomp@users.noreply.github.com>
2024-06-07 00:48:21 +0530

    Time: 68 ms (11.68%), Space: 46.3 MB (6.29%) - LeetHub

--- 0846-hand-of-straights/0846-hand-of-straights.java
+++ 0846-hand-of-straights/0846-hand-of-straights.java
@@ -18,18 +18,6 @@ class Solution {
                         count.put(keyValue+i, count.get(keyValue+i)-1);
                     }
                 }
-                // if(count.getOrDefault(keyValue+1,0)==0 || count.getOrDefault(keyValue+2,0)==0){
-                //     //System.out.println("Value of keyValue: " + keyValue);
-                //     return false;
-                // }
-                // else{
-                //     System.out.println("Value of keyValue: " + keyValue);
-                //     count.put(keyValue, count.get(keyValue)-1);
-                //     System.out.println("Value of 2: " + count.get(keyValue+1));
-                //     count.put(keyValue+1, count.get(keyValue+1)-1);
-                //     System.out.println("Value of 2: " + count.get(keyValue+1));
-                //     count.put(keyValue+2, count.get(keyValue+2)-1);
-                // }
             }
         }
         return true;
