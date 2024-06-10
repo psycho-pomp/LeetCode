90605fa95898667c320bd45c6eea2b9ad44c4650
Abhishek Anand <52618538+psycho-pomp@users.noreply.github.com>
2024-06-10 18:39:44 +0530

    Time: 0 ms (100.00%), Space: 10.1 MB (78.64%) - LeetHub

--- 1051-height-checker/1051-height-checker.cpp
+++ 1051-height-checker/1051-height-checker.cpp
@@ -2,9 +2,8 @@ class Solution {
 public:
     int heightChecker(vector<int>& heights) {
         int n = heights.size();
-           vector<int> sort_arr(n);
-        copy(cbegin(heights), cend(heights), begin(sort_arr));
-sort(begin(sort_arr), end(sort_arr));
+        vector<int> sort_arr = heights;
+        sort(sort_arr.begin(), sort_arr.end());
         int count=0;
         for(int i=0;i<n;i++){
             if(sort_arr[i]!=heights[i]){
