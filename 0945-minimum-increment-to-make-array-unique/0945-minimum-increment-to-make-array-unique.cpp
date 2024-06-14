6993c70525196fc3a7b8615725ceaea6b8833aac
Abhishek Anand <52618538+psycho-pomp@users.noreply.github.com>
2024-06-15 00:42:55 +0530

    Time: 118 ms (65.94%), Space: 69.1 MB (79.13%) - LeetHub

--- 0945-minimum-increment-to-make-array-unique/0945-minimum-increment-to-make-array-unique.cpp
+++ 0945-minimum-increment-to-make-array-unique/0945-minimum-increment-to-make-array-unique.cpp
@@ -3,16 +3,16 @@ public:
     int minIncrementForUnique(vector<int>& nums) {
         sort(nums.begin(),nums.end());
         int res=0;
-        //cout<<nums[0]<<",";
+        
         for(int i=1;i<nums.size();i++){
             if(nums[i]<=nums[i-1]){
                 
                 res+=(nums[i-1]+1-nums[i]);
                 nums[i]=nums[i-1]+1;
-                //cout<<nums[i]<<",";
+                
             }
         }
-        //cout<<endl;
+        
         return res;
         
     }
