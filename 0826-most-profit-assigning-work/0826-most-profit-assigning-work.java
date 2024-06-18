fecd351bc3d93b5c33b51e320c88ac5728ca10bc
Abhishek Anand <52618538+psycho-pomp@users.noreply.github.com>
2024-06-19 01:27:02 +0530

    Time: 9 ms (97.09%), Space: 45.7 MB (49.15%) - LeetHub

--- 0826-most-profit-assigning-work/0826-most-profit-assigning-work.java
+++ 0826-most-profit-assigning-work/0826-most-profit-assigning-work.java
@@ -1,29 +1,23 @@
 class Solution {
-
-    public int maxProfitAssignment(
-        int[] difficulty,
-        int[] profit,
-        int[] worker
-    ) {
-        // Find maximum ability in the worker array.
-        int maxAbility = Arrays.stream(worker).max().getAsInt();
-        int[] jobs = new int[maxAbility + 1];
-
-        for (int i = 0; i < difficulty.length; i++) {
-            if (difficulty[i] <= maxAbility) {
-                jobs[difficulty[i]] = Math.max(jobs[difficulty[i]], profit[i]);
+    public int maxProfitAssignment(int[] difficulty, int[] profit, int[] worker) {
+        int max_ability=Arrays.stream(worker).max().getAsInt(); 
+        int[] hash=new int[max_ability+1];
+        for(int i=0;i<difficulty.length;i++){
+            if(difficulty[i]<=max_ability){
+                hash[difficulty[i]]=Math.max(hash[difficulty[i]],profit[i]);
             }
         }
-
-        // Take maxima of prefixes.
-        for (int i = 1; i <= maxAbility; i++) {
-            jobs[i] = Math.max(jobs[i], jobs[i - 1]);
+        //System.out.println(max_ability);
+        for(int i=1;i<max_ability+1;i++){
+            hash[i]=Math.max(hash[i],hash[i-1]);
+            //System.out.print(hash[i]+",");
         }
-
-        int netProfit = 0;
-        for (int ability : worker) {
-            netProfit += jobs[ability];
+        //System.out.println();
+        int result=0;
+        for(int value:worker){
+            result+=hash[value];
         }
-        return netProfit;
+        return result;
+        
     }
 }
