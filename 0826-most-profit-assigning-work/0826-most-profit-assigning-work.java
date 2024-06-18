b91dc13986142f11c2ca16e6da4314a266ca6563
Abhishek Anand <52618538+psycho-pomp@users.noreply.github.com>
2024-06-19 01:28:35 +0530

    Time: 152 ms (18.81%), Space: 46.1 MB (18.57%) - LeetHub

--- 0826-most-profit-assigning-work/0826-most-profit-assigning-work.java
+++ 0826-most-profit-assigning-work/0826-most-profit-assigning-work.java
@@ -1,18 +1,27 @@
 class Solution {
     public int maxProfitAssignment(int[] difficulty, int[] profit, int[] worker) {
-        int max_ability=Arrays.stream(worker).max().getAsInt(); 
-        int[] hash=new int[max_ability+1];
+        int max_difficulty=Arrays.stream(difficulty).max().getAsInt(); 
+        int[] hash=new int[max_difficulty+1];
         for(int i=0;i<difficulty.length;i++){
-            if(difficulty[i]<=max_ability){
-                hash[difficulty[i]]=Math.max(hash[difficulty[i]],profit[i]);
-            }
+            hash[difficulty[i]]=Math.max(hash[difficulty[i]],profit[i]);
         }
-        for(int i=1;i<max_ability+1;i++){
+        //System.out.println(max_difficulty);
+        for(int i=1;i<max_difficulty+1;i++){
             hash[i]=Math.max(hash[i],hash[i-1]);
+            //System.out.print(hash[i]+",");
         }
+        //System.out.println();
         int result=0;
         for(int value:worker){
-            result+=hash[value];
+            System.out.print(value+"-");
+            if(value<=max_difficulty){
+                result+=hash[value];
+                //System.out.println(hash[value]);
+            }
+            else{
+                result+=hash[max_difficulty];
+                //System.out.println(hash[max_difficulty]);
+            }
         }
         return result;
         
