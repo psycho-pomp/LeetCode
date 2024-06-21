218efe999f58e0f4aa228fa7abb3561978584c1f
Abhishek Anand <52618538+psycho-pomp@users.noreply.github.com>
2024-06-22 01:22:17 +0530

    Time: 3 ms (91.51%), Space: 48.1 MB (6.93%) - LeetHub

--- 1052-grumpy-bookstore-owner/1052-grumpy-bookstore-owner.java
+++ 1052-grumpy-bookstore-owner/1052-grumpy-bookstore-owner.java
@@ -1,28 +1,23 @@
 class Solution {
     public int maxSatisfied(int[] customers, int[] grumpy, int minutes) {
         int n=customers.length;
-        int maximumWindow=0,maxIdx=0;
+        int maximumWindow=0;
         for(int i=0;i<minutes;i++){
             if(grumpy[i]==1)
                 maximumWindow+=customers[i];
         }
         int currWindow=maximumWindow;
         for(int i=1;i<=n-minutes;i++){
-            //System.out.println(i+","+customers[i-1]);
             if(grumpy[i-1]==1){
                 currWindow-=customers[i-1];
             }
-            //System.out.println(i+","+currWindow);
             if(grumpy[i+minutes-1]==1){
                 currWindow+=customers[i+minutes-1];   
             }
-            //System.out.println(i+","+currWindow);
             if(currWindow>maximumWindow){
-                    maxIdx=i;
                     maximumWindow=currWindow;
             }
         }
-        //System.out.println(maxIdx+","+maximumWindow);
         int result=0;
         for(int i=0;i<n;i++){
             if(grumpy[i]==0){
